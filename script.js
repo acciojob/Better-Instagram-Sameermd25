39da7ae6189bb61752f012815463c4e5e5573fde
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-09-23 21:16:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+const allBoxes=document.querySelectorAll(".image");
+for(let i=0;i<allBoxes.length;i++){
+            allBoxes[i].addEventListener("dragstart",(e)=>{
+                e.dataTransfer.setData("text",e.target.id);
+            })
+            allBoxes[i].addEventListener("dragover",(e)=>{
+                e.preventDefault();
+            })
+            allBoxes[i].addEventListener("drop",(e)=>{
+              const dragEleId=e.dataTransfer.getData("text") ;
+              const dragEle=document.getElementById(dragEleId);
+              const dragBackground=window.getComputedStyle(dragEle).backgroundImage;
+
+              const dropEle=e.target//.closest('.div');
+              const dropBackground=window.getComputedStyle(dropEle).backgroundImage;
+                
+              dragEle.style.backgroundImage=dropBackground;
+              dropEle.style.backgroundImage=dragBackground;
+
+              let temp=dragEle.innerText;
+              dragEle.innerText=dropEle.innerText;
+              dropEle.innerText=temp;
+            })
+        }
